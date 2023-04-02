7dfb2ef700b38b6fb6cdbbfab2ee4856c46bddcc
Nick Platt <platt.nicholas@gmail.com>
2023-04-01 22:18:25 -0400

    feat: restore openai integration

--- app/lib/artifact_ai_web/live/create_live/index.ex
+++ app/lib/artifact_ai_web/live/create_live/index.ex
@@ -30,9 +30,8 @@ defmodule ArtifactAiWeb.CreateLive.Index do
 
   def handle_event("submit", %{"prompt" => params}, socket) do
     # todo: move this to a helper function
-    #        with {:ok, result} <- OpenAI.Images.generate(params["prompt"], size: "1024x1024"),
-    #             %{"data" => [%{"url" => url}]} <- result,
-    with url <- "https://picsum.photos/1000",
+    with {:ok, result} <- OpenAI.Images.generate(params["prompt"], size: "1024x1024"),
+         %{"data" => [%{"url" => url}]} <- result,
          {:ok, prompt} <- Prompts.create(socket.assigns.current_user, params),
          {:ok, image} <- Images.create(socket.assigns.current_user, prompt, %{url: url}) do
       {:noreply, redirect(socket, to: ~p"/e/#{shortid(prompt.id)}/#{shortid(image.id)}")}
